5adb185c1d9087dac593e19da7f18cb59c5a6a43
李辉辉 <176029@sh.lianjia.com>
2018-08-19 12:13:46 +0800

    项目初始化

---
+++ src/main/java/com/hhli/springbootstduy/SpringbootStduyApplication.java
@@ -0,0 +1,12 @@
+package com.hhli.springbootstduy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStduyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootStduyApplication.class, args);
+	}
+}
