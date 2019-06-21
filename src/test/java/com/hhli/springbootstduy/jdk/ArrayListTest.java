c86d940306bf85b8a331b8b8c46b49fe8f4cda67
李辉辉 <176029@sh.lianjia.com>
2019-06-21 09:05:56 +0800

    java集合学习

---
+++ src/test/java/com/hhli/springbootstduy/jdk/ArrayListTest.java
@@ -0,0 +1,15 @@
+package com.hhli.springbootstduy.jdk;
+
+/**
+ * @author 李辉辉
+ * @date 2019/6/18 8:52
+ * @description 数组列表测试
+ */
+public class ArrayListTest {
+
+    public static void main(String[] args) {
+        int x = Integer.MAX_VALUE /3 * 2 ;
+        int temp = x + x>>1;
+        System.out.println(temp);
+    }
+}
