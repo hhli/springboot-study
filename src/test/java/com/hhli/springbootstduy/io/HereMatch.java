a9d0a48373fbf41801a6e84c266df081fda803d0
李辉辉 <hhli_unique@163.com>
2019-07-25 13:46:44 +0800

    java正则测试

--- src/test/java/com/hhli/springbootstduy/io/HereMatch.java
+++ src/test/java/com/hhli/springbootstduy/io/HereMatch.java
@@ -1,11 +1,11 @@
 package com.hhli.springbootstduy.io;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author 李辉辉
@@ -22,9 +22,21 @@ public class HereMatch {
 
             InputStreamReader in = new InputStreamReader(new URL(urlString).openStream(), StandardCharsets.UTF_8);
 
+            //read contents into string builder
+            StringBuilder input = new StringBuilder();
+            int ch;
+            while((ch = in.read()) != -1){
+                input.append((char) ch);
+            }
 
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
+            String patternString = "<a\\s+href\\s*=\\s*(\"[^\"]*\"[^\\s>]*)\\s*>";
+            Pattern pattern  = Pattern.compile(patternString, Pattern.CASE_INSENSITIVE);
+            Matcher matcher = pattern.matcher(input);
+            while(matcher.find()){
+                String match = matcher.group();
+                System.out.println(match);
+            }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
