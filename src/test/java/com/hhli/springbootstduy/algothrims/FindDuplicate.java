f3fcc490baf036e00d1102890039c186ae208d61
李辉辉 <hhli_unique@163.com>
2019-12-24 10:59:16 +0800

    发现重复数

--- src/test/java/com/hhli/springbootstduy/algothrims/FindDuplicate.java
+++ src/test/java/com/hhli/springbootstduy/algothrims/FindDuplicate.java
@@ -26,52 +26,53 @@ public class FindDuplicate {
 
 
     public static int findDuplicate(int[] nums) {
-        //int begin = 1;
-        //int end = nums.length-1;
-        //
-        //while (begin<end){
-        //    int times = 0;
-        //    int middle = (begin + end)>>>1;
-        //    for (int num : nums) {
-        //        if(num<middle){
-        //            times++;
-        //        }
-        //    }
-        //
-        //    if(times>=middle){
-        //        end = middle-1;
-        //    }else {
-        //        begin = middle;
-        //    }
-        //
-        //    System.out.println("begin=" + begin + ":end=" + end);
-        //
-        //}
-        //
-        //return begin;
+        int begin = 1;
+        int end = nums.length-1;
 
-
-        int len = nums.length;
-        int left = 1;
-        int right = len - 1;
-        while (left < right) {
-             int mid = left + (right - left + 1) / 2;
-            //int mid = (left + right + 1) >>> 1;
-            int counter = 0;
+        while (begin<end){
+            int times = 0;
+            int middle = (begin + end + 1)/2;
             for (int num : nums) {
-                if (num < mid) {
-                    counter += 1;
+                if(num<middle){
+                    times++;
                 }
             }
-            if (counter >= mid) {
-                // 如果小于 4 的个数等于 4 或者更多
-                // 那么重复的数一定位于 1、2、3
-                right = mid - 1;
-            } else {
-                left = mid;
+
+            if(times>=middle){
+                end = middle-1;
+            }else {
+                begin = middle;
             }
+
+            System.out.println("begin=" + begin + ":end=" + end);
+
         }
-        return left;
+
+        return begin;
+
+
+        //int len = nums.length;
+        //int left = 1;
+        //int right = len - 1;
+        //while (left < right) {
+        //     int mid = left + (right - left + 1) / 2;
+        //    //int mid = (left + right + 1) >>> 1;
+        //    int counter = 0;
+        //    for (int num : nums) {
+        //        if (num < mid) {
+        //            counter += 1;
+        //        }
+        //    }
+        //    if (counter >= mid) {
+        //        // 如果小于 4 的个数等于 4 或者更多
+        //        // 那么重复的数一定位于 1、2、3
+        //        right = mid - 1;
+        //    } else {
+        //        left = mid;
+        //    }
+        //}
+        //
+        //return left;
     }
 
 }
--- src/test/java/com/hhli/springbootstduy/stream/MainLambda.java
+++ src/test/java/com/hhli/springbootstduy/stream/MainLambda.java
@@ -1,37 +1,32 @@
 package com.hhli.springbootstduy.stream;
 
-import lombok.ToString;
-
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * @author 李辉辉
  * @date 2019/12/15 16:35
  * @description
  */
 public class MainLambda {
-    //public static void main(String[] args) {
-    //    new Thread(
-    //            () -> System.out.println("Lambda Thread run()")
-    //    ).start();;
-    //}
-
-    Runnable r1 = () -> { System.out.println(this); };
-    Runnable r2 = () -> { System.out.println(toString()); };
     public static void main(String[] args) {
-        //new MainLambda().r1.run();
-        //new MainLambda().r2.run();
-
-        Map<Integer, String> paramMap = new HashMap<>();
-        paramMap.put(1, "one");
-        paramMap.put(2, "two");
-        paramMap.merge(1, "1", (v1, v2)-> v1 + v2);
-
-        System.out.println(paramMap);
+        new Thread(
+                () -> System.out.println("Lambda Thread run()")
+        ).start();;
     }
 
-    @Override
-    public String toString() { return "Hello Hoolee"; }
+    //Runnable r1 = () -> { System.out.println(this); };
+    //Runnable r2 = () -> { System.out.println(toString()); };
+    //public static void main(String[] args) {
+    //    //new MainLambda().r1.run();
+    //    //new MainLambda().r2.run();
+    //
+    //    Map<Integer, String> paramMap = new HashMap<>();
+    //    paramMap.put(1, "one");
+    //    paramMap.put(2, "two");
+    //    paramMap.merge(1, "1", (v1, v2)-> v1 + v2);
+    //
+    //    System.out.println(paramMap);
+    //}
+    //
+    //@Override
+    //public String toString() { return "Hello Hoolee"; }
 
 }
