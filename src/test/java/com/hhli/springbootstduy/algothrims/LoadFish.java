d1f7add86f64d57495a436d35498fe83ede78a73
李辉辉 <176029@sh.lianjia.com>
2019-12-26 11:39:07 +0800

    捞鱼问题

---
+++ src/test/java/com/hhli/springbootstduy/algothrims/LoadFish.java
@@ -0,0 +1,13 @@
+package com.hhli.springbootstduy.algothrims;
+
+/**
+ * @author 李辉辉
+ * @date 2019/12/25 21:41
+ * @description 将 30 条鱼放入 10 个桶中，每个桶可以放 0-10 条，总共有多少种方法
+ */
+public class LoadFish {
+
+    public static void main(String[] args) {
+
+    }
+}
