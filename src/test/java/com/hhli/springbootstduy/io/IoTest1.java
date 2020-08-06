adb5f31ceba15e179d55769084d0eb1ad97c701e
lihuihui <lihuihui@ke.com>
2020-08-06 22:08:17 +0800

    dns测试

--- src/test/java/com/hhli/springbootstduy/io/IoTest1.java
+++ src/test/java/com/hhli/springbootstduy/io/IoTest1.java
@@ -1,14 +1,8 @@
 package com.hhli.springbootstduy.io;
 
-import org.apache.commons.lang.StringUtils;
-
-import java.io.*;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.List;
-import java.util.Scanner;
-import java.util.stream.Stream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author 李辉辉
@@ -18,28 +12,36 @@ import java.util.stream.Stream;
 public class IoTest1 {
 
     public static void main(String[] args) throws IOException {
-        InputStream in = new FileInputStream(new File("D:\\github\\springboot-study\\src\\test\\resources\\example.txt"));
-        int byteAvailable = in.available();
-        System.out.println("byte length=" + byteAvailable);
-        in.close();
+        //InputStream in = new FileInputStream(new File("D:\\github\\springboot-study\\src\\test\\resources\\example.txt"));
+        //int byteAvailable = in.available();
+        //System.out.println("byte length=" + byteAvailable);
+        //in.close();
+        //
+        //String content = new String(Files.readAllBytes(Paths.get("")), StandardCharsets.UTF_8);
+        //List<String> lines = Files.readAllLines(Paths.get(""));
+        //Stream<String> fileStream =  Files.lines(Paths.get(""));
+        //Scanner scanner = new Scanner(new File(""));
+        //while (scanner.hasNextLine()){
+        //    String line = scanner.nextLine();
+        //}
+        //
+        //InputStream inputStream = new FileInputStream("");
+        //try(BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))){
+        //    String line;
+        //    while (StringUtils.isNotBlank(line=reader.readLine())){
+        //        System.out.println(line);
+        //    }
+        //
+        //}
+        while (true){
+            InetAddress inetAddress = InetAddress.getByName("cms.gateway.ke.com");
+            System.out.println(inetAddress.getHostAddress());
 
-        String content = new String(Files.readAllBytes(Paths.get("")), StandardCharsets.UTF_8);
-        List<String> lines = Files.readAllLines(Paths.get(""));
-        Stream<String> fileStream =  Files.lines(Paths.get(""));
-        Scanner scanner = new Scanner(new File(""));
-        while (scanner.hasNextLine()){
-            String line = scanner.nextLine();
-        }
-
-        InputStream inputStream = new FileInputStream("");
-        try(BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))){
-            String line;
-            while (StringUtils.isNotBlank(line=reader.readLine())){
-                System.out.println(line);
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-
         }
-
-
     }
 }
